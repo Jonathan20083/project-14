e4f4a788239d5a0eac462ced2353ca4386b3adf3
Jonathan20083 <103178313+Jonathan20083@users.noreply.github.com>
2022-05-23 18:48:36 +0930

    Add files via upload

--- Project-Template-C14-main/sketch.js
+++ Project-Template-C14-main/sketch.js
@@ -69,9 +69,7 @@ function draw() {
   if (arrowGroup.isTouching(redB)) {
     
     redB.destroyEach();
-    redB.destroy();
-    redB.Each();
-    ballon.destroyEach();
+   
     
     arrowGroup.destroyEach();
     score=score+1;
@@ -146,9 +144,7 @@ function pinkBalloon() {
   arrow.lifetime = 100;
   arrow.scale = 0.3;
   
-  arrowGroup.addGroup(arrow);
-  arrow.add(arrowGroup);
-  arrowGroup.add();
+  
   arrowGroup.add(arrow);
    
 }
